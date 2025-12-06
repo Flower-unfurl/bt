67a290d34d15cb808ddcddec55fe2e21a03410e0
wiki-1911 <datteo192004@gmail.com>
2025-12-06 10:14:48 +0700

    ...

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -6,8 +6,9 @@ function App() {
     <div className="App">
         <label>Username: </label>
         <input id='txtusername' ></input>
-        <label>Mật khẩu: </label>
-        <input id='txtmatkhau' ></input>
+        <label>Password: </label>
+        <input id='txtpassword' ></input>
+        <button id='btnlogin'>Login</button>
     </div>
   );
 }
